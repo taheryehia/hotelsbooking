df7733a2739b72bd12ccdf89cdf1a76439b5f90e
taher <taheryahia22@gmail.com>
2026-07-30 18:47:28 +0300

    Z

--- cms/config/database.js
+++ cms/config/database.js
@@ -3,15 +3,19 @@ module.exports = ({ env }) => {
   return {
     connection: {
       client: 'postgres',
-      connection: connectionString || {
-        host: env('DATABASE_HOST', 'aws-1-us-east-1.pooler.supabase.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'postgres'),
-        user: env('DATABASE_USERNAME', 'postgres.pzmrhykxhzzjlvzrihzw'),
-        password: env('DATABASE_PASSWORD', 'USN3uIIDQQjZcyTH'),
-        ssl: { rejectUnauthorized: false },
-      },
-      ssl: { rejectUnauthorized: false },
+      connection: connectionString
+        ? {
+            connectionString,
+            ssl: { rejectUnauthorized: false },
+          }
+        : {
+            host: env('DATABASE_HOST', 'aws-1-us-east-1.pooler.supabase.com'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'postgres'),
+            user: env('DATABASE_USERNAME', 'postgres.pzmrhykxhzzjlvzrihzw'),
+            password: env('DATABASE_PASSWORD', 'USN3uIIDQQjZcyTH'),
+            ssl: { rejectUnauthorized: false },
+          },
       pool: { min: 0, max: 10 },
     },
   };
