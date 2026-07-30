8ae36f675b777d6095261666cead9921258ed860
taher <taheryahia22@gmail.com>
2026-07-30 19:30:15 +0300

    Z

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,22 +1,14 @@
-module.exports = ({ env }) => {
-  const connectionString = env('DATABASE_POSTGRES_URL_NON_POOLING') || env('DATABASE_URL');
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: connectionString
-        ? {
-            connectionString,
-            ssl: { rejectUnauthorized: false },
-          }
-        : {
-            host: env('DATABASE_HOST', 'aws-1-us-east-1.pooler.supabase.com'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'postgres'),
-            user: env('DATABASE_USERNAME', 'postgres.pzmrhykxhzzjlvzrihzw'),
-            password: env('DATABASE_PASSWORD', 'USN3uIIDQQjZcyTH'),
-            ssl: { rejectUnauthorized: false },
-          },
-      pool: { min: 0, max: 10 },
+      host: env('DATABASE_HOST', 'aws-1-us-east-1.pooler.supabase.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres.pzmrhykxhzzjlvzrihzw'),
+      password: env('DATABASE_PASSWORD', 'USN3uIIDQQjZcyTH'),
+      ssl: { rejectUnauthorized: false },
     },
-  };
-};
+    pool: { min: 0, max: 10 },
+  },
+});
